f623b71fedc5fa0e5e9235b87c2787a5c04f63c5
anthonybchung <11941@coderacademy.edu.au>
2022-09-28 14:28:41 +1000

    fix api key when running pm2

--- routes/weather.js
+++ routes/weather.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const dotenv = require('dotenv');
 
-dotenv.config({
-  path: './config/config.env',
-});
-
-const WeatherKey = process.env.APPID;
+const APPID = require('../config/secret');
 
 router.get('/', async (req, res) => {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${WeatherKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=Sydney,au&APPID=${APPID}`
   );
 
   const weatherData = await response.json();
+
   res.status(200).json({
     status: true,
     data: weatherData,
